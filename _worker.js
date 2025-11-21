3fea24e6e85a727bd74011de6d8943591089452e
xiao <1809489542@qq.com>
2025-11-21 11:21:24 +0800

    添加用户信息接口

--- _worker.js
+++ _worker.js
@@ -125,9 +125,9 @@ async function 整理优选列表(api, hostName, subscription) {
 		clearTimeout(timeout);
 	}
 
-	// const userInfo = await 获取用户信息(hostName, subscription);
+	const userInfo = await 获取用户信息(hostName, subscription);
 
-	const newAddressesapi = await 整理(newapi);
+	const newAddressesapi = await 整理(userInfo + newapi);
 
 	// 返回处理后的结果
 	return newAddressesapi;
